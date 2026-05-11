21d5915668da7685d4099b957ef552a820c003ab
nko2024 <155769271+nko2024@users.noreply.github.com>
2026-05-10 21:32:11 -0400

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -21,15 +21,9 @@ const data = [
 // DIMENSIONS
 // =====================================
 
-const margin = {
-  top: 30,
-  right: 20,
-  bottom: 50,
-  left: 60
-};
-
+const margin = { top: 20, right: 20, bottom: 40, left: 50 };
 const width = 700 - margin.left - margin.right;
-const height = 360 - margin.top - margin.bottom;
+const height = 400 - margin.top - margin.bottom;
 
 // =====================================
 // SVG
